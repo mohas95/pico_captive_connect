19f0f53c809f97889aabf6a2cebe1198409ccaff
mohas95 <mohas.debbagh95@gmail.com>
2025-10-03 18:57:58 -0400

    more interesting demo code

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,24 @@
 #include "pico_captive_connect.h"
 #include <cstring>
 #include <cstdio>
+#include <cstdlib>
+#include "pico/rand.h"
+
+
 
 static absolute_time_t next_pub = 0;
+
+float random_temp(){
+    uint32_t r = get_rand_32();
+    return 20.0f + (r % 1000) / 100.0f;
+}
 
 
 int main() {
     stdio_init_all();
     sleep_ms(1000);
+
+    srand(to_ms_since_boot(get_absolute_time()));
 
     net_init();
 
@@ -22,9 +33,12 @@ int main() {
 
         // Publish only if MQTT connected and it's time
         if (mqtt_is_connected() && absolute_time_diff_us(get_absolute_time(), next_pub) < 0) {
-            const char* msg = "{\"temp\":23.5}";
+            float temp = random_temp();
+            char msg[64];
+            snprintf(msg, sizeof(msg), "{\"temp\":%.2f}", temp);
+
             if (publish_mqtt("sensors/temp", msg, strlen(msg))) {
-                printf("[APP] Published temp message\n");
+                printf("[APP] Published temp message: %.2f\n", temp);
                 next_pub = make_timeout_time_ms(1000); // normal period
             } else {
                 printf("[APP] Publish failed, backing off\n");
